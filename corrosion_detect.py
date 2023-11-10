f65ba956e3b9bfa5fa002467fc82df042bf5f2aa
Fenix-LAB <fenixcr.123@gmail.com>
2023-11-09 20:01:18 -0600

    wip

--- corrosion_detect.py
+++ corrosion_detect.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 import cv2
 
 # load model
-model = YOLO("best.pt")
+model = YOLO("model.pt")
 
 # init camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) # Para cambiar la camara
 
 # loop
 while True:
@@ -14,13 +14,16 @@ while True:
     ret, frame = cap.read()
 
     # resultados use the model for detection and segmentation
-    resultados = model.predict(frame, imgsz = 640, conf = 0.5)
+    resultados = model.predict(frame, imgsz = 640, conf = 0.7) # 0
+
+    # Confidence conf = 0.7 X
+    # conf = 0.9   OK
 
     # creat a new frame with the results
     anotaciones = resultados[0].plot()
 
     # show the frame
-    cv2.imshow("DETECCION Y SEGMENTACION", anotaciones)
+    cv2.imshow("DETECCION DE CORROCION Y SEGMENTACION", anotaciones)
 
     # break if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
