35bb443569ea87b3f1d933da38c5e15b26430967
Fenix-LAB <fenixcr.123@gmail.com>
2024-04-28 16:19:02 -0600

    Add real-time rust detection using YOLO model and webcam

--- corrosion_detect.py
+++ corrosion_detect.py
@@ -1,10 +1,13 @@
-# Importamos las librerias
-from ultralytics import YOLO # dentro de ultralytics esta YOLO
-import cv2 # libreriaa de procesamiento de imagenes
+from ultralytics import YOLO
+import cv2
+
+"""
+This a simple script uses the YOLO model to detect rust in real time using the webcam.
+
+"""
 
 # load model
 model = YOLO("model.pt")
-
 # init camera
 cap = cv2.VideoCapture(0) # Para cambiar la camara
 
@@ -14,17 +17,13 @@ while True:
     ret, frame = cap.read()
 
     # resultados use the model for detection and segmentation
-    resultados = model.predict(frame, imgsz = 640, conf = 0.7) 
-    # La red neuronal solo recibe imagenes de 640x640 
-
-    # Confidence conf = 0.7 X
-    # conf = 0.9   OK
+    result = model.predict(frame, imgsz = 640, conf = 0.7) 
 
     # creat a new frame with the results
-    anotaciones = resultados[0].plot()
+    detection = result[0].plot()
 
     # show the frame
-    cv2.imshow("DETECCION DE CORROCION Y SEGMENTACION", anotaciones)
+    cv2.imshow("Rusty Detection", detection)
 
     # break if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
